bb6f411ba332ba7fac65a4c01f7f8a7e3fc79978
Oleg Konan <olegkonanff@gmail.com>
2020-10-01 22:08:21 +0300

    task is done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || array[0] == undefined) {
+        return 0;
+    } else {
+        const sortedArr = array.sort(function(a, b) {
+            return a - b;
+        })
+        return sortedArr[0];
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array[0] == undefined) {
+        return 0;
+    } else {
+        const sortedArr = array.sort(function(a, b) {
+            return a - b;
+        })
+        return sortedArr[array.length - 1];
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array[0] == undefined) {
+        return 0;
+    } else {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        return sum / array.length;
+    }
 }
